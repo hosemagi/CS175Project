60ba61be1262163e9b1af5a4d29b2c1af6918139
Preston <prestonclewis@gmail.com>
2011-05-22 15:35:02 -0700

    added time profiling to solver

--- src/lowerDivision.py
+++ src/lowerDivision.py
@@ -3,7 +3,7 @@ Created on May 9, 2011
 
 @author: will
 '''
-
+from datetime import datetime
 from Numberjack import *
 import Mistral
 
@@ -36,6 +36,13 @@ model.add([ sum((term[c] == 4) for c in range(total_classes))<=4 ])
 #model.add( Minimise(term[c] for c in range(max_terms)<=max_terms) )
 
 msolver = Mistral.Solver(model)
+
+
+print "Solving..."
+starttime = datetime.now()
 msolver.solve()
+endtime = datetime.now()
+elapsed = endtime - starttime
+print "Solution took " + str(elapsed)
 
 print term
